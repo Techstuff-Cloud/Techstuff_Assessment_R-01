ff095b90bf3cc8597cb1716e9bc722e038660e79
Atul Malviya <65947031+Atmalviya@users.noreply.github.com>
2024-07-08 10:37:54 +0000

    solution for problem 1

--- src/assessments/01/solution.js
+++ src/assessments/01/solution.js
@@ -1,3 +1,11 @@
+
+// Write code here
 export const maximizeProfit = (prices) => {
-  // Write code here
+  let totalProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      totalProfit += prices[i] - prices[i - 1]
+    }
+  }
+  return totalProfit;
 };
